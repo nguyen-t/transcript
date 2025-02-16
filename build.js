cdb6d83cbf66391f395942e3039522bd33869344
nguyen-t <5609219+nguyen-t@users.noreply.github.com>
2025-02-16 17:07:16 -0600

    Changed build script from sh to js

---
+++ build.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+import { execSync } from 'node:child_process';
+
+const WHISPER_CPP = 'https://github.com/ggerganov/whisper.cpp';
+const WHISPER_DIR = 'whisper.cpp';
+const BUILD_DIR = `${WHISPER_DIR}/build`;
+const CMAKE_OPTIONS = [
+  '',
+  'CMAKE_POSITION_INDEPENDENT_CODE=ON',
+  'BUILD_SHARED_LIBS=OFF',
+  'GGML_CCACHE=OFF',
+  'GGML_OPENMP=OFF',
+  ...process.argv.slice(2),
+].join(' -D').trim();
+
+try {
+  execSync(`git clone ${WHISPER_CPP}`, {
+    'stdio': ['pipe', 'pipe', 'ignore'],
+  });
+} catch (error) {
+}
+try {
+  execSync(`cmake ${WHISPER_DIR} -B ${BUILD_DIR} ${CMAKE_OPTIONS}`);
+  execSync(`cmake --build ${BUILD_DIR} --config Release `);
+} catch (error) {
+  console.log(error);
+}
