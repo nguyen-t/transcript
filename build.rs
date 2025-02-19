076729916e92f3a24a9774b3f6877949b80b3cfc
nguyen-t <5609219+nguyen-t@users.noreply.github.com>
2025-02-19 00:31:58 -0600

    More build script changes

--- build.rs
+++ build.rs
@@ -6,24 +6,22 @@ use std::{env, fs};
 fn main() {
   let root = env::current_dir().unwrap();
 
-  println!("Directory: {}", root.display());
-  println!("cargo:rustc-link-search=native={}", "whisper.cpp/build/src");
-  println!("cargo:rustc-link-search=native={}", "whisper.cpp/build/ggml/src");
+  println!("cargo:rustc-link-search=native={}", root.join("whisper.cpp/build/src").display());
+  println!("cargo:rustc-link-search=native={}", root.join("whisper.cpp/build/ggml/src").display());
   println!("cargo:rustc-link-lib=static:+whole-archive={}", "whisper");
   println!("cargo:rustc-link-lib=static:+whole-archive={}", "ggml-base");
   println!("cargo:rustc-link-lib=static:+whole-archive={}", "ggml-cpu");
   println!("cargo:rustc-link-lib=static:+whole-archive={}", "ggml");
-  println!("cargo:rerun-if-changed={}", "whisper");
-  println!("cargo:rerun-if-changed={}", "wrapper.h");
+  println!("cargo:rerun-if-changed={}", root.join("whisper").display());
+  println!("cargo:rerun-if-changed={}", root.join("wrapper.h").display());
 
-  fs::create_dir("bindings")
-    .expect("Directory bindings already created");
+  fs::create_dir("bindings").ok();
   bindgen::Builder::default()
     .header("wrapper.h")
-    .clang_arg("-Iwhisper.cpp/ggml/include")
+    .clang_arg(format!("-I{}", root.join("whisper.cpp/ggml/include").display()))
     .generate()
     .expect("Unable to generate bindings")
-    .write_to_file("bindings/whisper.rs")
+    .write_to_file(root.join("bindings/whisper.rs"))
     .expect("Couldn't write bindings!");
 
   napi_build::setup();
